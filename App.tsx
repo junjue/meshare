d44cb1312a1eb52511a6e8e756aca521db386cde
JJ <junjuewang@gmail.com>
2021-06-28 11:28:31 -0400

    init project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <p>hey there!</p>
       <StatusBar style="auto" />
     </View>
   );
